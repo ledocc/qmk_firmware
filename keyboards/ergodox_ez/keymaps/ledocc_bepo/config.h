a2d6e19ad7e3cc6a229ab6589451d00e6d95334f
David Callu <callu.david@gmail.com>
2019-05-16 07:21:55 +0200

    feat(ergodox ez): add ledoc bepo

---
+++ keyboards/ergodox_ez/keymaps/ledocc_bepo/config.h
@@ -0,0 +1,7 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#undef TAPPING_TERM
+#define TAPPING_TERM 200
+
